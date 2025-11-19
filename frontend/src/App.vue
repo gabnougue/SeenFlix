3a40a878c6ce5e5fde53012a7f504b7fb60e2976
Gabin Nougué <gabnougue@gmail.com>
2025-11-19 17:29:52 +0100

    feat: add frontend routing and navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,16 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+
+<script setup>
+import Navbar from "./components/Navbar.vue"
+</script>
+
+<style>
+body {
+  margin: 0;
+  background: #f5f5f5;
+  font-family: sans-serif;
+}
+</style>
