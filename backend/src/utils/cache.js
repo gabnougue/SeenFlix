187bc86e1f1223817d32419365705a01759f59fb
Gabin Nougué <gabnougue@gmail.com>
2025-11-19 14:35:10 +0100

    feat(cache): add LRU caching for TMDB search results

---
+++ backend/src/utils/cache.js
@@ -0,0 +1,8 @@
+import { LRUCache } from "lru-cache";
+
+const cache = new LRUCache({
+  max: 100,                 // nombre max d'entrées
+  ttl: 1000 * 60 * 10       // 10 minutes
+});
+
+export default cache;
