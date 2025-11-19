ef9cd7fe4664eb03bea19deb75b77075642a23b7
Gabin Nougué <gabnougue@gmail.com>
2025-11-19 16:46:41 +0100

    feat: setup Vue.js frontend skeleton

---
+++ frontend/src/store/user.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: null,     // email + id du backend
+    token: null     // JWT stocké ici
+  }),
+  actions: {
+    setUser(user, token) {
+      this.user = user
+      this.token = token
+    },
+    logout() {
+      this.user = null
+      this.token = null
+      localStorage.removeItem('jwt')
+    }
+  }
+})
